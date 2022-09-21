ad52d255eebd05ffebeb5ad688f0722cd9a9a67a
Matt-Fr <matthieu_franck@orange.fr>
2022-09-21 11:08:54 +0200

    added style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import argentBankLogo from "../assets/argentBankLogo.png";
+
 const Header = () => {
   return (
     <nav className="main-nav">
-      <Link className="main-nav-logo">
-        <img
-          src={argentBankLogo}
-          alt="logo"
-          className="main-nav-logo-image.png"
-        />
+      <Link className="main-nav-logo ">
+        <img src={argentBankLogo} alt="logo" className="main-nav-logo-image" />
       </Link>
+      <div>
+        {/* <FontAwesomeIcon icon="fa-solid fa-circle-user" /> */}
+        <Link className="main-nav-item">Sign In</Link>
+      </div>
     </nav>
   );
 };
