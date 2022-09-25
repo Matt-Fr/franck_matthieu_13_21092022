e405491cc6dc1e5f403ad0eef27887782582c07f
Matt-Fr <matthieu_franck@orange.fr>
2022-09-25 12:11:21 +0200

    deleted useless pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home";
