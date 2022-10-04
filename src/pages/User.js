01e5f82610d8757bf312ca84b99aa3efb5ae5410
Matt-Fr <matthieu_franck@orange.fr>
2022-10-04 22:39:59 +0200

    added input user page

--- src/pages/User.js
+++ src/pages/User.js
@@ -4,51 +4,89 @@ import { useDispatch, useSelector } from "react-redux";
 
 const User = () => {
   const { isLoading, user } = useSelector((store) => store.user);
+  const { firstName, lastName } = user;
   const dispatch = useDispatch();
   const [userData, setUserData] = useState({
     firstName: user?.firstName || "",
     lastName: user?.lastName || "",
   });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!firstName || !lastName) {
+      alert("please fill out all fields");
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+    console.log(`${name} ${value}`);
+  };
+
   return (
-    <main class="main bg-dark">
-      <div class="header">
+    <main className="main bg-dark">
+      <div className="header">
         <h1>
           Welcome back
           <br />
-          {`${user.firstName} ${user.lastName}!`}
+          {`${firstName} ${lastName}!`}
         </h1>
-        <button class="edit-button">Edit Name</button>
+        <form action="">
+          <div className="input-wrapper">
+            <label htmlFor="firstName">Firstname</label>
+            <input
+              type="text"
+              name="firstName"
+              value={userData.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="LastName">Lastname</label>
+            <input
+              type="text"
+              name="lastName"
+              value={userData.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="edit-button" type="submit" disabled={isLoading}>
+            {isLoading ? "isLoading..." : "Save Name"}
+          </button>
+        </form>
+        <button className="edit-button">Edit Name</button>
       </div>
-      <h2 class="sr-only">Accounts</h2>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-          <p class="account-amount">$2,082.79</p>
-          <p class="account-amount-description">Available Balance</p>
+      <h2 className="sr-only">Accounts</h2>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+          <p className="account-amount">$2,082.79</p>
+          <p className="account-amount-description">Available Balance</p>
         </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Savings (x6712)</h3>
-          <p class="account-amount">$10,928.42</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
         </div>
       </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p class="account-amount">$184.30</p>
-          <p class="account-amount-description">Current Balance</p>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+          <p className="account-amount">$10,928.42</p>
+          <p className="account-amount-description">Available Balance</p>
         </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section className="account">
+        <div className="account-content-wrapper">
+          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+          <p className="account-amount">$184.30</p>
+          <p className="account-amount-description">Current Balance</p>
+        </div>
+        <div className="account-content-wrapper cta">
+          <button className="transaction-button">View transactions</button>
         </div>
       </section>
     </main>
