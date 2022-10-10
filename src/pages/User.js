8e400256aa4afe93206ac88dc6c20d5d79a32291
Matt-Fr <matthieu_franck@orange.fr>
2022-10-10 23:18:55 +0200

    added account components

--- src/pages/User.js
+++ src/pages/User.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Account from "../components/Account";
 import {
   getUser,
   updateUser,
@@ -92,36 +93,21 @@ const User = () => {
         )}
       </div>
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <Account
+        accountName="Argent Bank Checking (x8349)"
+        amount="$2,082.79"
+        balance="Available Balance"
+      />
+      <Account
+        accountName="Argent Bank Savings (x6712)"
+        amount="$10,928.42"
+        balance="Available Balance"
+      />
+      <Account
+        accountName="Argent Bank Credit Card (x8349)"
+        amount="$184.30"
+        balance="Current Balance"
+      />
     </main>
   );
 };
